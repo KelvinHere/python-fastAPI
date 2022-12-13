dceda528069fc52339c6241266fc9b0c2074a3c0
kelvinhere <kwardeu@gmail.com>
2022-12-13 18:16:56 +0000

    FEAT: Add get operations

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # python-fastAPI
 Having a look at fastAPI
 
-## Hello World
-Simple setup of FastAPI - Hello world.
+## Operations
+I have implemented different operations to get used to FastAPI.  The input type of these operations can be constrained by defining a type for example. 
+
+<pre># Will take any value
+@app.get("/users/{user_id}")
+    async def read_item(user_id):</pre>
+
+<pre># Expects an integer to be supplied
+@app.get("/users/{user_id}")
+    async def read_item(user_id: int):</pre>
+
+<pre># Constrained to values in Enum
+@app.get("/specific/{enumHere}")
+    async def read_item(specificValue :enumHere):</pre>
 
 ## Start Uvicorn server
 ```uvicorn main:app --reload```
 --reload: make the server restart after code changes.  Development use only.
+
+## Useful paths
+
+```/docs``` Lets you see all path operation decorators and schemas etc.
+```/redocs``` Generates documentation from the OpenAPI definitions
--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
+class LanguageEnum(str, Enum):
+    python = "python"
+    java = "java"
+    javascript = "javascript"
+
+
 @app.get("/")
-async def root():
-        return {"message": "Hello World"}
+def home_screen():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "user me selected", "another": "field"}
+
+
+@app.get("/users/{user_id}")
+async def read_item(user_id: int):
+    return {"user_id": user_id}
+
+
+@app.get("/language/{language_name}")
+async def read_language_name(language_name: LanguageEnum):
+    if language_name is LanguageEnum.python:
+        return {"language_name": language_name, "message": "that got coded quickly"}
+
+    if language_name is LanguageEnum.java:
+        return {"language_name": language_name, "message": "that is some strict typing"}
+
+    if language_name is LanguageEnum.javascript:
+        return {"language_name": language_name, "message": "you got any libraries? React maybe?"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path:str):
+    return {"file_path": file_path}
